201d2d5267849f06a7ae155f035d1eff3d7a401a
Tnjonny <79794963+Tnjonny@users.noreply.github.com>
2023-01-16 19:41:19 +0000

    Added modal functionality

--- script.js
+++ script.js
@@ -2,5 +2,22 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnOpen = document.querySelector('.show-modal');
+const btnOpen = document.querySelectorAll('.show-modal');
 const btnClose = document.querySelector('.close-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpen.length; i++) {
+  btnOpen[i].addEventListener('click', openModal);
+}
+
+btnClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
