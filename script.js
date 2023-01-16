b999d46e96bf8577b5f448afc8afd69e72ec8eea
Tnjonny <79794963+Tnjonny@users.noreply.github.com>
2023-01-16 19:11:34 +0000

    Modal Project

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnOpen = document.querySelector('.show-modal');
+const btnClose = document.querySelector('.close-modal');
